c9e01dea7c07162d8c74b473d80365369da43de4
AsifMostafiz1 <mostafizur15-8001@diu.edu.bd>
2022-05-19 22:39:03 +0600

    Product Services Added

---
+++ lib/services/product_services.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import '../model/products.dart';
+
+class ProductServices {
+  // final queryParameter ={
+  //   'limit':'10',
+  //   'offset': (1*10).toString()
+  // };
+
+  Future<ProductSearch?> getProducts(int pageNumber) async {
+    String apiUrl =
+        "https://panel.supplyline.network/api/product/search-suggestions/?limit=10&offset=${pageNumber * 10}";
+    try {
+      var response = await http.get(
+        Uri.parse(apiUrl),
+        headers: {'Content-Type': 'application/json'},
+      );
+
+      if (response.statusCode == 200) {
+        return ProductSearch.fromJson(
+          jsonDecode(
+            utf8.decode(response.bodyBytes),
+          ),
+        );
+      } else {
+        throw "${response.statusCode}";
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
